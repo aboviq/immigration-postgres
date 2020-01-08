347a30b2024f603b92de0c322ab593963a7ff53d
Joakim Carlstein <joakim@aboviq.com>
2020-01-08 10:44:15 +0100

    build: simplify/fix db config

--- migrations/20191219123300_fourth.js
+++ migrations/20191219123300_fourth.js
@@ -7,10 +7,6 @@ exports.up = async () => {
 	await pool.query(`
 		DROP TABLE immigration_postgres;
 	`);
-	const {rows} = await pool.query(`SELECT * FROM migrations`);
-	console.log(
-		rows.map(row => `${row.name} - ${row.status} - ${row.date.toISOString()}`).join('\n')
-	);
 };
 
 exports.down = () => {};
